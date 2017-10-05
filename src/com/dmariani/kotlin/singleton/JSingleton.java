95cf7238f05235401546c23c82a504a73c48f4b7
Danielle Mariani <danielle.mariani@gmail.com>
2017-10-05 18:22:50 -0500

    Add Java Singleton class

---
+++ src/com/dmariani/kotlin/singleton/JSingleton.java
@@ -0,0 +1,24 @@
+package com.dmariani.kotlin.singleton;
+
+/**
+ * @author danielle.mariani
+ */
+public class JSingleton {
+
+    private static JSingleton instance;
+
+    private JSingleton() {
+    }
+
+    public static JSingleton getInstance() {
+        if (instance == null) {
+            instance = new JSingleton();
+        }
+
+        return instance;
+    }
+
+    public void print() {
+        System.out.println("Hello World from Singleton");
+    }
+}
